70c8cbcbd04d96e7328ac1b9015eea8935a8d710
Emmanuel <emmanuelmathew1995@gmail.com>
2024-08-23 18:24:47 +0530

    ...

---
+++ src/Ecommerce.Application/Features/Authentication/Notifications/UserRegistered/UserRegisteredNotification.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Ecommerce.Application.Features.Authentication.Notifications.UserRegistered
+{
+    public class UserRegisteredNotification : INotification
+    {
+        public string Username { get; }
+        public string Email { get; }
+        public string EmailConfirmationToken { get; }
+
+        public UserRegisteredNotification(string username, string email, string emailConfirmationToken)
+        {
+            Username = username;
+            Email = email;
+            EmailConfirmationToken = emailConfirmationToken;
+        }
+    }
+}
---
+++ src/Ecommerce.Domain/Settings/AppSettings.cs
@@ -0,0 +1,51 @@
+﻿namespace Ecommerce.Domain.Settings
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Gets or sets the URL for confirmation email.
+        /// </summary>
+        public string ConfirmationEmailUrl { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the email sender address.
+        /// </summary>
+        public string EmailSenderAddress { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the Azure Communication Service connection string.
+        /// </summary>
+        public string AzureCommunicationServiceConnection { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the default connection string.
+        /// </summary>
+        public string DefaultConnection { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the JWT settings.
+        /// </summary>
+        public JwtSettings Jwt { get; init; } = default!;
+    }
+
+    /// <summary>
+    /// Represents the JWT settings.
+    /// </summary>
+    public record JwtSettings
+    {
+        /// <summary>
+        /// Gets or sets the JWT key.
+        /// </summary>
+        public string Key { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the JWT issuer.
+        /// </summary>
+        public string Issuer { get; init; } = default!;
+
+        /// <summary>
+        /// Gets or sets the JWT audience.
+        /// </summary>
+        public string Audience { get; init; } = default!;
+    }
+}
